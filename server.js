03a20989a68a8f43eeb582d2e7b5e3b16db700f0
Chris Chua <chris.sirhc@gmail.com>
2011-02-01 23:59:33 -0500

    Initial commit. Some working extraction from Facebook Graph API.

---
+++ server.js
@@ -0,0 +1,80 @@
+/**
+ * This is where it all begins
+ */
+var express = require("express");
+var request = require("request");
+var redis = require("redis"),
+  rclient = redis.createClient();
+
+var app = express.createServer();
+
+/** debugging **/
+app.set('env', 'production');
+
+// Setup the server
+app.configure(function () {
+  app.use(express.conditionalGet());
+  app.use(express.cache());
+  app.use(express.gzip());
+  /** if it's a file, serve it **/
+  app.use(express.compiler({src: __dirname + '/public', enable: ['less']}));
+  app.use(express.staticProvider(__dirname + '/public'));
+
+  app.use(express.logger());
+  app.use(express.bodyDecoder());
+  app.use(express.methodOverride());
+});
+
+app.configure('production', function() {
+  app.use(express.errorHandler());
+  app.listen(80);
+  console.log("Listening to 80");
+});
+
+app.configure('development', function() {
+  app.listen(3000);
+  console.log("Listening to 3000");
+});
+
+
+app.get('/fetch', function (req, res, next) {
+  request({
+    uri: "https://graph.facebook.com/search?q=%22georgia+tech%22&type=event&limit=50"
+  },
+    function (error, response, body) {
+      var bodyObj, i, currId, hashArr;
+      if (!error && response.statusCode == 200) {
+        hashArr = [];
+        bodyObj = JSON.parse(body);
+        /** Care about the order later **/
+        for (i = bodyObj.data.length; i--;) {
+          currId = bodyObj.data[i]['id'];
+
+          bodyObj.data[i]['start_time'] = new Date(bodyObj.data[i]['start_time']).getTime();
+          bodyObj.data[i]['end_time'] = new Date(bodyObj.data[i]['end_time']).getTime();
+
+          rclient.sadd("eventslist", currId);
+          rclient.hmset(currId, bodyObj.data[i]);
+        }
+        res.send("Fetched");
+      }
+    });
+});
+
+app.get('/list', function (req, res, next) {
+  rclient.smembers("eventslist", function (err, result) {
+    res.send(result.join("\n"));
+  });
+});
+
+app.get('/:id', function (req, res, next) {
+  rclient.hgetall(req.params.id, function (err, result) {
+    res.send(result, { 'Content-Type': 'text/plain' });
+  });
+});
+
+// Store a database of connected/authenticated users
+
+// Process the users like a cron job
+
+// Wait for requests from the mobile web application and serve them
